8971d27f0dc560793780f11885826b721510795c
Jalilov <javohirjalilov777@gmail.com>
2021-09-08 19:05:36 +0500

    create url get_data

--- api_tut/urls.py
+++ api_tut/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
+from data_api.views import get_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get_data/', get_data),
 ]
--- data_api/views.py
+++ data_api/views.py
@@ -1,2 +1,4 @@
-def get_data():
-    pass
+from django.http import HttpResponse
+
+def get_data(request):
+    return HttpResponse("get_data page")
