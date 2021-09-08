322e7f1e3b22418597fa13c23086f5427f985850
ZARIFMAC <uzbzarif@gmail.com>
2021-09-08 22:52:34 +0900

    remove

--- api_tut/urls.py
+++ api_tut/urls.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def salom(request):
-    return HttpResponse('HI')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hi/',salom)
 ]
