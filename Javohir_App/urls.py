a744e61d40de3903fa0a104092c7c8d8004eca95
Jalilov <javohirjalilov777@gmail.com>
2021-09-09 22:39:16 +0500

    find odd and even number

--- Javohir_App/urls.py
+++ Javohir_App/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('sum/', views.sum_two_digits),
+    path('mult/', views.digits_multiplication),
+    path('odd_even/', views.find_odd_even)
 ]
--- Javohir_App/views.py
+++ Javohir_App/views.py
@@ -10,4 +10,46 @@ def sum_two_digits(request):
 		print(a,b)
 		response = {"SUM":int(a)+int(b)}
 		return JsonResponse(response)
+
+def digits_multiplication(request):
+
+	if request.method == 'GET':
+		a = request.GET.get("a",1)
+		b = request.GET.get("b",1)
+		print(a,b)
+		response = {"SUM":int(a)*int(b)}
+		return JsonResponse(response)
+
+def find_odd_even(request):
+	if request.method == 'GET':
+		a = int(request.GET.get("a",0))
+		b = int(request.GET.get("b",0))
+		if a == 0:
+			if b == 0:
+				response = {"a":"zero number","b":"zero number"}
+			else:
+				if b%2 == 1:
+					response = {"a":"zero number","b":"odd"}
+				else:
+					response = {"a":"zero number","b":"odd"}
+		elif b == 0:
+			if a == 0:
+				response = {"a":"zero number","b":"zero number"}
+			else:
+				if a%2 == 1:
+					response = {"b":"zero number","a":"odd"}
+				else:
+					response = {"b":"zero number","a":"odd"}
+		else:
+			if a%2 == 1:
+				if b%2 == 1:
+					response = {"b":"odd","a":"odd"}
+				else:
+					response = {"b":"even","a":"odd"}
+			else:
+				if b%2 == 1:
+					response = {"b":"odd","a":"even"}
+				else:
+					response = {"b":"even","a":"even"}
 		
+		return JsonResponse(response)
