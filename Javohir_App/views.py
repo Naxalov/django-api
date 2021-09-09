f0efb52a15e7cc56420ac31eaab59cefc8678b34
Jalilov <javohirjalilov777@gmail.com>
2021-09-09 22:19:23 +0500

    sum two digits

--- Javohir_App/views.py
+++ Javohir_App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse, response
 # Create your views here.
+
+def sum_two_digits(request):
+
+	if request.method == 'GET':
+		a = request.GET.get("a",0)
+		b = request.GET.get("b",0)
+		print(a,b)
+		response = {"SUM":int(a)+int(b)}
+		return JsonResponse(response)
+		
--- api_tut/urls.py
+++ api_tut/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.http import HttpResponse
 
 
@@ -9,5 +9,6 @@ from data_api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hi/', views.get_data),
+    path('javohir_app/',include('Javohir_App.urls'))
 
 ]
