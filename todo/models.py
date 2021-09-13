2d30fd3aa2cba960a1928a65168bc47bb075d580
ZARIFMAC <uzbzarif@gmail.com>
2021-09-13 22:29:53 +0900

    create todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Todo_list(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    complete = models.BooleanField(default=False)
+    user_id = models.CharField(max_length=10)
